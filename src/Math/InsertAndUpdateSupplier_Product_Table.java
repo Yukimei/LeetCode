8464cfe6660bfc861c7e555cb835c751b8b74668
yingxuemei <alicemeiyingxue@126.com>
2016-05-09 17:44:30 -0400

    test

--- src/Math/InsertAndUpdateSupplier_Product_Table.java
+++ src/Math/InsertAndUpdateSupplier_Product_Table.java
@@ -1,15 +1,20 @@
 package Math;
 
 public class InsertAndUpdateSupplier_Product_Table {
-	public void readfiles(String directoryPath) {
-		
-	}
+    public void readfiles(String directoryPath) {
 
-	public boolean update(String supplierId, String productId, int quantity) {
-		return true;
-	}
-	
-	public boolean checkSupplierIdExist(String supplierId, ) {
-		return true;
-	}
+    }
+
+    public boolean update(String supplierId, String productId, int quantity) {
+        return true;
+    }
+
+    public boolean checkSupplierIdExist(String supplierId,) {
+        return true;
+    }
+
+
+
+
+
 }
